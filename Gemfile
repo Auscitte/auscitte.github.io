661fa6e22d790552532527f76288f75c038bc956
Ry Auscitte <auscitte@users.noreply.github.com>
2021-04-22 10:42:24 +0000

    liquid includes for figures, ruby plugin

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source "https://rubygems.org"
 
-gem 'github-pages', group: :jekyll_plugins
+gem 'jekyll', group: :jekyll_plugins
 gem 'jekyll-sitemap'
 gem 'jekyll-paginate'
-
+gem "kramdown-parser-gfm"
+gem  'dimensions'
+gem 'yaml'
---
+++ _plugins/reslist-gen.rb
@@ -0,0 +1,41 @@
+###################################################################################
+# Jekyll plugin that creates and maintains a yaml file (/_data/resolutions.yml)   #
+# containing dimentions for each image file stored in /resources/images.          #
+#                                                                                 #
+# Authors: Ry Auscitte                                                            #
+#                                                                                 #
+###################################################################################
+
+require 'dimensions'
+require 'yaml'
+
+module Jekyll
+  class JekyllResolutionsListGenerator < Generator
+  	safe true
+    	priority :high
+    	
+  	def generate(site)
+  		data_name = "resolutions"
+      		paths = Dir["#{site.source}/resources/images/*"]
+      		needsRefresh = false
+      		paths.each do |f|
+        		if File.file?(f)
+        			filename = f.match(/[^\/]*$/)[0]
+        			if not site.data[data_name].key?(filename)
+        				needsRefresh = true
+        				dims = { "width" => Dimensions.width(f) , "height" => Dimensions.height(f) }
+        				site.data[data_name][filename] = dims
+        			else
+        				puts site.data[data_name][filename]
+        			end	
+        		end
+        	end
+        	
+        	if needsRefresh
+        		File.open("#{site.source}/_data/#{data_name}.yml","w") do |file|
+   				file.write(site.data[data_name].to_yaml)
+			end
+        	end
+      	end
+  end  
+end  
